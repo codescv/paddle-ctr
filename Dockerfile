e12163dddd8e3e12006562be85e94d00a4b5af8f
kelin <kelin@pinduoduo.com>
2018-12-17 15:26:12 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-#FROM ccr.ccs.tencentyun.com/pdd-tf/paddle:develop-py27
 FROM codescv/paddle:develop-py27
 RUN mkdir -p /paddle-ctr
 COPY . /paddle-ctr
--- ps2.sh
+++ ps2.sh
@@ -1,2 +1,2 @@
 CMD='python /paddle-ctr-test/src/train.py --is_local 0 --role pserver --endpoints 127.0.0.1:6174,127.0.0.1:6175 --trainers 2 --sparse_feature_dim 1000001 --current_endpoint 127.0.0.1:6175'
-docker run --rm --name paddle-ps2 -v $PWD/../data:/data -v $PWD:/paddle-ctr-test --network=host -it ccr.ccs.tencentyun.com/pdd-tf/paddle-ctr:latest $CMD
+docker run --rm --name paddle-ps2 -v $PWD/../data:/data -v $PWD:/paddle-ctr-test --network=host -it codescv/paddle-ctr:latest $CMD
